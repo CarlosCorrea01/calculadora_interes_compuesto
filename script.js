bee9aef97556161cae67a607e80c09dc49c3a0e0
Correa Carlos Fabricio <fabricorrea011015@hotmail.com>
2022-04-18 19:39:51 -0300

    Desafio complementario N°1 - MINI CALCULADORA

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 //Calculadora de interes compuesto con aportacion mensual y calculadora de interes compuesto sin aportacion.
+// 18/04/22 desafio entregable - operaciones entre dos numeros
 
-function intCompSinAporte (capitalInicial,interesAcumulado){
-    let montoSinAporte = capitalInicial*interesAcumulado.toFixed(2)
-    console.log(montoSinAporte)
+let numeroUno = 0;
+let numeroDos = 0;
+let operacion = ""
+alert("Hola! En este pequeño algoritmo realizaremos una mini calculadora para el desafio complementario N°1")
+
+let pregunta = prompt("Listo? pon SI para continuar o NO para terminar, por favor ingresa la respuesta en mayusculas")
+let confirmacion = ""
+
+while(pregunta!="NO"){
+    if (pregunta==="SI"){
+        numeroUno = parseFloat(prompt("Ingresa el primer numero"));
+        numeroDos = parseFloat(prompt("Ingresa el segundo numero"));
+        operacion = prompt("Que operacion realizaras? ingresa - ; + ; * ; /");
+        alert("Usted ingreso:" + " "+ numeroUno+" "+operacion+" "+numeroDos+"."+" El resultado sera:")
+        if (operacion==="-" ){
+            alert(numeroUno-numeroDos);
+        }else if (operacion === "+"){
+            alert(numeroUno+numeroDos);
+        }else if(operacion==="*"){
+            alert(numeroUno*numeroDos);
+        }else if (operacion === "/"){
+            alert(numeroUno/numeroDos);
+        }else {
+            alert("El valor ingresado no es correcto, por favor intentalo nuevamente")
+        }
+    }else{
+        alert("Ingresa SI o NO en mayusculas, vuelve a intentarlo")
+    }
+    pregunta = prompt("Deseas realizar otra operacion? SI o NO")
 }
-function intCompConAporte (capitalInicial,interesAcumulado,aporte,i){
-    let montoConAporte = ((capitalInicial*interesAcumulado)+(((aporte*(interesAcumulado-1))/i))).toFixed(2)
-    console.log(montoConAporte)
-}
-
-
-let capitalInicial = parseFloat(prompt("ingrese capital inicial $"))
-let plazo = parseFloat(prompt("Ingrese plazo de inversion/ahorro en AÑOS"))
-let intereses = parseFloat(prompt("Ingrese tasa de rendimiento estimada ANUAL"))
-let aporte = parseFloat (prompt("Ingrese aporte mensual"))
-
-let i = intereses/1200
-let n = plazo*12
-let interesAcumulado = Math.pow ((i+1),n)
-
-do {
-    if 
-} while (condition);
