99a50933dfe902decb03d9bf6e8fecff612905b1
Correa Carlos Fabricio <fabricorrea011015@hotmail.com>
2022-04-19 20:30:04 -0300

    calculadora de interes compuesto

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 //Calculadora de interes compuesto con aportacion mensual y calculadora de interes compuesto sin aportacion.
-// 18/04/22 desafio entregable - operaciones entre dos numeros
 
-let numeroUno = 0;
-let numeroDos = 0;
-let operacion = ""
-alert("Hola! En este pequeño algoritmo realizaremos una mini calculadora para el desafio complementario N°1")
+function interesCompuesto (capital,interes,plazo,aporte){
+    let i = interes/1200
+    let plazoMes = plazo*12
+    let intAcumulado = Math.pow((1+i),plazoMes)
 
-let pregunta = prompt("Listo? pon SI para continuar o NO para terminar, por favor ingresa la respuesta en mayusculas")
-let confirmacion = ""
+    return ((capital*intAcumulado) + ((aporte*(intAcumulado-1))/i))
+}
 
-while(pregunta!="NO"){
+alert("Hola! Nos alegra que estes interesado en ahorrar! a continuacion te pediremos inforamcion para calcuar tu ahorro")
+let pregunta = prompt("Deseas continuar? ingresa SI o NO, ingresa la respuesta en MAYUSCULAS")
+
+while(pregunta!=="NO"){
     if (pregunta==="SI"){
-        numeroUno = parseFloat(prompt("Ingresa el primer numero"));
-        numeroDos = parseFloat(prompt("Ingresa el segundo numero"));
-        operacion = prompt("Que operacion realizaras? ingresa - ; + ; * ; /");
-        alert("Usted ingreso:" + " "+ numeroUno+" "+operacion+" "+numeroDos+"."+" El resultado sera:")
-        if (operacion==="-" ){
-            alert(numeroUno-numeroDos);
-        }else if (operacion === "+"){
-            alert(numeroUno+numeroDos);
-        }else if(operacion==="*"){
-            alert(numeroUno*numeroDos);
-        }else if (operacion === "/"){
-            alert(numeroUno/numeroDos);
-        }else {
-            alert("El valor ingresado no es correcto, por favor intentalo nuevamente")
-        }
-    }else{
-        alert("Ingresa SI o NO en mayusculas, vuelve a intentarlo")
+        let capital = parseFloat(prompt("Ingrese CAPITAL INICIAL"))
+        let interes = parseFloat(prompt("Ingrese interes ANUAL"))
+        let plazo = parseFloat(prompt("Ingrese plazo del ahorro en AÑOS"))
+        let aporte = parseFloat(prompt("Ingrese APORTE mensual"))
+
+        let capitalFinal = interesCompuesto (capital,interes,plazo,aporte).toFixed(2);
+        alert ("Su capital final sera:"+" $"+capitalFinal)
+        alert ("Los intereses generados fueron:"+ " $"+(capitalFinal- (aporte*(plazo*12))).toFixed(2))
+        alert ("En total los aportes suman:"+ " $"+(aporte*(plazo*12)))
+        alert("¡GRACIAS POR ELEGIRNOS!")   
+    }else {
+        alert("Disculpe pero la opcion ingresada es invalida, por favor! ingrese SI o NO en MAYUSCYLAS.")
     }
-    pregunta = prompt("Deseas realizar otra operacion? SI o NO")
+    pregunta = prompt("Desea realizar otro calculo? ingresa SI o NO, ingresa la respuesta en MAYUSCULAS")
 }
