e147ab2ea8084d122f9b9f99ebc8307bc8a867e9
Correa Carlos Fabricio <fabricorrea011015@hotmail.com>
2022-04-20 18:44:37 -0300

    version 2.0 - calculadora de intereses compuestos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Calculadora de interes compuesto con aportacion mensual y calculadora de interes compuesto sin aportacion.
+//Calculadora de interes compuesto con aportacion mensual.
 
 function interesCompuesto (capital,interes,plazo,aporte){
     let i = interes/1200
@@ -9,7 +9,8 @@ function interesCompuesto (capital,interes,plazo,aporte){
 }
 
 alert("Hola! Nos alegra que estes interesado en ahorrar! a continuacion te pediremos inforamcion para calcuar tu ahorro")
-let pregunta = prompt("Deseas continuar? ingresa SI o NO, ingresa la respuesta en MAYUSCULAS")
+
+let pregunta = prompt("Deseas continuar? ingresa SI o NO").toUpperCase()
 
 while(pregunta!=="NO"){
     if (pregunta==="SI"){
@@ -19,12 +20,12 @@ while(pregunta!=="NO"){
         let aporte = parseFloat(prompt("Ingrese APORTE mensual"))
 
         let capitalFinal = interesCompuesto (capital,interes,plazo,aporte).toFixed(2);
-        alert ("Su capital final sera:"+" $"+capitalFinal)
-        alert ("Los intereses generados fueron:"+ " $"+(capitalFinal- (aporte*(plazo*12))).toFixed(2))
-        alert ("En total los aportes suman:"+ " $"+(aporte*(plazo*12)))
+        alert ("Su capital final sera: $"+capitalFinal)
+        alert ("Los intereses generados fueron: $"+(capitalFinal - (aporte*(plazo*12))-(capital)).toFixed(2))
+        alert ("En total los aportes suman: $"+(aporte*(plazo*12)))
         alert("¡GRACIAS POR ELEGIRNOS!")   
     }else {
-        alert("Disculpe pero la opcion ingresada es invalida, por favor! ingrese SI o NO en MAYUSCYLAS.")
+        alert("Disculpe pero la opcion ingresada es invalida, por favor! ingrese SI o NO.")
     }
-    pregunta = prompt("Desea realizar otro calculo? ingresa SI o NO, ingresa la respuesta en MAYUSCULAS")
+    pregunta = prompt("Desea realizar otro calculo? ingresa SI o NO").toUpperCase()
 }
