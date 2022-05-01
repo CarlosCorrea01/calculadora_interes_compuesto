2f2f7f65834251824ea8dd063fe71521c486db0d
Correa Carlos Fabricio <fabricorrea011015@hotmail.com>
2022-05-01 18:42:54 -0300

    prototipo N°1

--- script.js
+++ script.js
@@ -61,43 +61,41 @@ while(pregunta !== "NO"){
     }
     pregunta = prompt("Desea registrarce? ingrese si o no").toUpperCase()
 } 
-let = preguntaUsu = ""
+
 let ingUsu = prompt("Ingrese su usuario")
 let ingContra= prompt("ingrese su contraseña")
 let busqueda = arrayUsuarios.indexOf(arrayUsuarios.find(usuarios => usuarios.usuario===ingUsu && usuarios.contrasena===ingContra))
 let usuarioX = arrayUsuarios.find(usuarios => usuarios.usuario===ingUsu && usuarios.contrasena===ingContra)
 
-while(){
-    
-}
-alert("Bienvenido "+busqueda.nombre+ " "+ busqueda.apellido)
-/*
-if(((busqueda.planes).length)==0){
-    alert("No tiene ningun plan de ahorro/Inversion,Crearemos uno")
-    
-    busqueda.nuevoAhorroInversion(
-    prompt("Cual es el motivo del ahorro/inversion. Ej: Auto neuvo"),
-    parseFloat(prompt("Ingrese capital inicial si lo tiene")),
-    parseFloat(prompt("Que rendimiento ANUAL tendra su ahorro/Inversion?")),
-    parseInt(prompt("Cuanto tiempo planea ahorrar/Invertir? en AÑOS")),
-    parseFloat(prompt("Ingrese capital que aportara cada mes"))
-    )
-    alert("Genail tiene "+(busqueda.planes).length+" plan/es creados")
-    preguntaPlan = prompt("DeASDSAD")
-   
-}   */
-
-
-
-
-/*
-arrayUsuarios.forEach((usuarios,indice)=>{
-    let ingUsu = prompt("Ingrese su usuario")
-    let ingContra= prompt("ingrese su contraseña")
-    let indiceIng = arrayUsuarios.indexOf(arrayUsuarios.find(usuarios => usuarios.usuario===ingUsu && usuarios.contrasena===ingContra))
-    if (indice === indiceIng){
-        console.log("Bienvenido "+usuarios.nombre + " "+ usuarios.apellido)
-    }else{
-        console.log("no existe")
+if (busqueda != -1){
+    alert("Bienvenido "+usuarioX.nombre + " " + usuarioX.apellido)
+    if(((usuarioX.planes).length)==0){
+        let crearPlan=prompt("No tiene ningun plan de ahorro/Inversion, necesita crar uno. Desea continuar SI o NO").toUpperCase()
+        while (crearPlan!=="SALIR"){
+            if(crearPlan==="SI"){
+                usuarioX.nuevoAhorroInversion(
+                    prompt("Cual es el motivo del ahorro/inversion. Ej: Auto neuvo"),
+                    parseFloat(prompt("Ingrese capital inicial si lo tiene")),
+                    parseFloat(prompt("Que rendimiento ANUAL tendra su ahorro/Inversion?")),
+                    parseInt(prompt("Cuanto tiempo planea ahorrar/Invertir? en AÑOS")),
+                    parseFloat(prompt("Ingrese capital que aportara cada mes"))
+                    )
+                    alert("Genail tiene "+(usuarioX.planes).length+" plan/es creados")
+            }else if(crearPlan==="NO"){
+                alert("Necesita crear por lo menos un plan")
+            }else{
+                alert("Seleccion no valida")
+            }
+           
+            crearPlan = prompt("Si ya creo un plan ingrese SALIR, de lo contrario ingrese SI para crar un plan").toUpperCase();     
+        }
+            
+       
+        (usuarioX.planes).forEach(planes=>{{
+            alert("El capital final para el objetivo "+planes.objetivo+" sera de $"+planes.calcularCap()+" esto en "+planes.plazo*12+" meses,"+
+            "con un capital inicial de $"+planes.capital+" y aportes mensuales de $"+planes.aporte)
+        }})
     }
-})*/
+}else{
+    alert("No encontramos el usuario")
+}
