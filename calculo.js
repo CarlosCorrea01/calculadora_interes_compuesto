93901911ce9fd0d99f18215f85a72485cc59c0f4
Correa Carlos Fabricio <fabricorrea011015@hotmail.com>
2022-05-18 15:43:04 -0300

    prueba 3

--- calculo.js
+++ calculo.js
@@ -16,4 +16,32 @@ class AhorroInversion{
     }
 
 }
+const capInicial =  document.getElementById("capInicial")
+const tasaIntereses = document.getElementById("tasaIntereses")
+const plazo = document.getElementById("plazo")
+const aporte = document.getElementById("aporte")
+const objetivo = document.getElementById("objetivo")
+const calculo = document.getElementById("calculo")
 
+calculo.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    
+    const Ahorro1 = new AhorroInversion(objetivo.value,capInicial.value,tasaIntereses.value,plazo.value,aporte.value) 
+    let planes = document.getElementById("planes")
+    planes.innerHTML += `
+                         <div class="planesContenedor mt-2 d-flex justify-content-center align-items-center">
+                           <div class="mt-3">
+                             <p>
+                               Objetivo: <span>${Ahorro1.objetivo}</span>
+                                | Capital: <span>$${Ahorro1.capital}</span>
+                                | Tasa de Interes: <span>${Ahorro1.interes}%</span>
+                                | Plazo: <span>${Ahorro1.plazo*12} meses</span>
+                                | Capital Final: <span class="spanCapFin">$${Ahorro1.calcularCap()}</span>
+                             </p>
+                           </div>
+                         </div>   
+    
+    `
+    calculo.reset()
+ })
+ 
--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 
-class AhorroInversion{
-    constructor(objetivo,capital,interes,plazo,aporte){
-        this.objetivo = objetivo
-        this.capital = capital;
-        this.interes = interes;
-        this.plazo = plazo;
-        this.aporte = aporte;
-    }
-    calcularCap(){
-        const i = this.interes/1200;
-        const plazoMes = this.plazo*12;
-        const intAcumulado = Math.pow((1+i),plazoMes)
-        
-        return (((this.capital*intAcumulado) + ((this.aporte*(intAcumulado-1))/i)).toFixed(2))
-    }
-
-}
 const user = document.getElementById("user")
 const password = document.getElementById("password")
 const login = document.getElementById("login")
 
 login.addEventListener('submit',(e)=>{
-    e.preventDefault()
     let busqueda = arrayUsuarios.indexOf(arrayUsuarios.find(usuarios => usuarios.usuario===user.value && usuarios.contrasena===password.value))
     let usuarioX = arrayUsuarios.find(usuarios => usuarios.usuario===user.value && usuarios.contrasena===password.value)
+    e.preventDefault()
     if(user.value ==="" || password.value ==="" || busqueda==-1){
         let msjAlert= document.getElementById("msjAlert")
         msjAlert.innerHTML ='<p style="color:red">Usuario y contraseña incorrectos</p>'
         login.reset()
     }else if(user.value!=="" && password.value !=="" && busqueda!=-1){
-     //window.open('pagusu.html')
+     window.open('secciones/panel.html','_self')
+     //let saludoIngreso = document.getElementById("saludoIngreso")
+     //saludoIngreso.innerHTML = `<p class="nomUsu">Bienvenido<span>${usuarioX.nombre} ${usuarioX.apellido}</span></p>`
      alert("Bienvenido "+usuarioX.nombre+ " "+ usuarioX.apellido)
     }else{
         alert("Operaciones no validas")
@@ -37,26 +22,6 @@ login.addEventListener('submit',(e)=>{
 })
 
 
-
-/*
-const usuario = document.getElementById("usuario");
-const contra = document.getElementById("contra");
-const ingresar = document.getElementById("ingresar");
-ingresar.addEventListener('click',()=>{   
-    let busqueda = arrayUsuarios.indexOf(arrayUsuarios.find(usuarios => usuarios.usuario===usuario.value && usuarios.contrasena===contra.value))
-    let usuarioX = arrayUsuarios.find(usuarios => usuarios.usuario===usuario.value && usuarios.contrasena===contra.value)
-    if(usuario.value ==="" || contra.value ==="" || busqueda==-1){
-        let mensaje = document.getElementById("logForm");
-        mensaje.innerHTML +='<p style="color:red">Usuario y contraseña incorrectos</p>'
-        setTimeout(function() {
-            location.reload();
-          }, 1000);
-    }else if(usuario.value!=="" && contra.value !=="" && busqueda!=-1){
-     location.href="pagusu.html"
-     let bienvenida = document.getElementById("navBienvenida")
-     bienvenida.innerHTML='<p>Bienvenido </p>'
-    }
-})*/
 
 
 
